0985184cfea0eb9ebcb0c70490e45b21fe80454d
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-10-24 09:29:48 +0200

    Added å to regex, split regex search, fixed len bugs

--- pairingScript.py
+++ pairingScript.py
@@ -4,8 +4,16 @@ import re
 import random
 import csv
 
-# set the folder name of submissions as the next argument when running the script
-filesPath = sys.argv[1]
+csvPath = "pairings_file.csv"
+
+if (len(sys.argv) > 1):
+	# set the folder name of submissions as the next argument when running the script
+	filesPath = sys.argv[1]
+else:
+	filesPath = input("Please input the path to the folder of the student submissions: ")
+if (len(sys.argv) > 2):
+	# set the output csv file
+	csvPath = sys.argv[2]
 
 # get all file names of current folder
 files = os.listdir(filesPath)
@@ -13,13 +21,12 @@ files = os.listdir(filesPath)
 # create empty dict of student names who have submitted
 names = {}
 
-# a-zæáéíóäëiöúàèììù
-
 # populate the dict
 for file in files:
-	# TODO: split regular expression and check that it has a result
-	studentName = re.search("[a-zæáéíóäëiöúàèììù]+", file.lower()).group()
-	names[studentName] = True
+	reResult = re.search("^[a-zæáéíóäëiöúàèììùå][a-zæáéíóäëiöúàèììùå]*", file.lower())
+	if (reResult):
+		studentName = reResult.group()
+		names[studentName] = True
 
 # create pairings dict
 pairings = {}
@@ -27,17 +34,15 @@ pairings = {}
 # populate pairings dictionary
 # there will be a leftover name in names
 # if there's an odd amount of names
-while(len(names) >= 2):
+while (len(names) >= 2):
 	choice1 = random.choice(tuple(names))
 	del names[choice1]
 	choice2 = random.choice(tuple(names))
 	del names[choice2]
 	pairings[choice1] = choice2
 
-# TODO: add remaining name to some group if applicable
-
 # write out csv file with pairings
-with open("pairings_file.csv", mode = "w") as pairings_file:
+with open(csvPath, mode = "w") as pairings_file:
 	writer = csv.writer(pairings_file, delimiter = ',')
 	for choice1 in pairings:
 		writer.writerow([choice1, pairings[choice1]])
